b83ba59d70e9039f3c0160c442d5eb28782d7e76
kyoung0311 <kyoung0311@lotte.net>
2021-04-26 18:08:20 +0900

    Initial commit

---
+++ microservice/composite-service/src/test/java/com/kmtp/compositeservice/CompositeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmtp.compositeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompositeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
