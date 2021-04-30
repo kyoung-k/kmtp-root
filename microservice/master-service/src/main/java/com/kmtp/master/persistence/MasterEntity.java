b6e8db8743055e11f9ab5f5e28e1b3779b92a51f
kyoung0311 <kyoung0311@lotte.net>
2021-04-30 19:14:59 +0900

    [KYoung] company commit

--- microservice/master-service/src/main/java/com/kmtp/master/persistence/MasterEntity.java
+++ microservice/master-service/src/main/java/com/kmtp/master/persistence/MasterEntity.java
@@ -14,10 +14,76 @@ public class MasterEntity {
 
     @Id
     private Long id;
-
     @Column("name")
     private String name;
-
     @Column("information")
     private String information;
+}
+
+@Data
+@Builder
+@Table("members")
+class MemberEntity {
+
+    @Id
+    private Long id;
+
+    private String email;
+    private String name;
+    private int age;
+    private String addres;
+}
+
+@Data
+@Builder
+@Table("items")
+class ItemEntity {
+
+    @Id
+    private Long id;
+    private Long masterId;
+    private String name;
+}
+
+@Data
+@Builder
+@Table("charges")
+class ChargeEntity {
+
+    @Id
+    private Long id;
+    private Long itemId;
+    private Long charge;
+}
+
+@Data
+@Builder
+@Table("goods")
+class GoodsEntity {
+
+    @Id
+    private Long id;
+    private Long masterId;
+    private String name;
+}
+
+@Data
+@Builder
+@Table("discounts")
+class DiscountEntity {
+
+    @Id
+    private Long id;
+    private Long masterId;
+    private String name;
+    private Long discount;
+}
+
+@Data
+@Builder
+@Table("itemgoods")
+class ItemGoodsEntity {
+
+    private Long itemId;
+    private Long goodsId;
 }
